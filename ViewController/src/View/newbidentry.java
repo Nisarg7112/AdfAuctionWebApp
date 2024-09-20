eada82f2a3dfad2f016b1802244b62044cb461fa
Nisarg Shah <109804208+Nisarg7112@users.noreply.github.com>
2024-09-20 17:30:02 +0530

    Bid Done

--- ViewController/src/View/newbidentry.java
+++ ViewController/src/View/newbidentry.java
@@ -8,6 +8,8 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import oracle.adf.model.BindingContext;
@@ -16,6 +18,7 @@ import oracle.adf.model.binding.DCBindingContainer;
 import oracle.adf.model.binding.DCDataControl;
 import oracle.adf.share.ADFContext;
 import oracle.adf.share.security.SecurityContext;
+import oracle.adf.view.rich.component.rich.RichPopup;
 import oracle.adf.view.rich.component.rich.input.RichInputText;
 import oracle.adf.view.rich.event.PopupCanceledEvent;
 import oracle.adf.view.rich.event.PopupFetchEvent;
@@ -30,38 +33,36 @@ import oracle.jbo.ViewObject;
 
 import org.apache.myfaces.trinidad.event.AttributeChangeEvent;
 
+@ManagedBean
+@SessionScoped
 public class newbidentry extends LoggedUserHelper{
-   
+    private RichPopup myPopup;
+    
     private Timestamp  time;
     private Integer     bidderid;
-    private RichInputText bidderId;
-    private RichInputText amount;
-    private RichInputText auctionid;
-
-    private BigDecimal aid;
-    private BigDecimal amou;
     private BigDecimal id;
+    private int amount;
+
     public newbidentry() {
     }
     
-    
-   public void popupfetch(PopupFetchEvent popupFetchEvent) {
-      
-        
-        BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
-        OperationBinding op = bindings.getOperationBinding("CreateInsert");
-        op.execute();
-            
-        // Add event code here...
-    }
+    public void popupfetch(PopupFetchEvent popupFetchEvent) {
+         
+          
+         BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
+          OperationBinding op = bindings.getOperationBinding("CreateInsert");
+           op.execute();
+               
+           // Add event code here...
+       }
 
-    public void popupcancel(PopupCanceledEvent popupCanceledEvent) {
-        // Add event code here...
-        BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
-        OperationBinding op = bindings.getOperationBinding("Rollback");
-        
-        op.execute();
-    }
+       public void popupcancel(PopupCanceledEvent popupCanceledEvent) {
+           // Add event code here...
+           BindingContainer bindings = BindingContext.getCurrent().getCurrentBindingsEntry();
+           OperationBinding op = bindings.getOperationBinding("Rollback");
+           
+           op.execute();
+       }
 
     public String OKentrybid(){
        //  Add event code here...
@@ -72,22 +73,28 @@ public class newbidentry extends LoggedUserHelper{
                     DCBindingContainer bindings = (DCBindingContainer) bctx.getCurrentBindingsEntry();
                     DCDataControl dataControl = bindings.getDataControl();
                     ApplicationModule am = (ApplicationModule)dataControl.getDataProvider();
+                    
+        ViewObject AuctionVo=am.findViewObject("G4AuctionVO2_1");
+        Row auction=AuctionVo.first();
+        BigDecimal auctionid = (BigDecimal) auction.getAttribute("Auctionid");
+        
                     ViewObject BidVO = am.findViewObject("G4BidVO2");
                     Row newBid = BidVO.createRow();
                     
                     bidderid = getLoggedInUserId();
                     time= Timestamp.valueOf(LocalDateTime.now());
                     id= BigDecimal.valueOf(bidderid);
-                    Object value=amount.getValue();
-                    amou=(BigDecimal) value;
                     
-                    Object val=auctionid.getValue();
-                    aid=(BigDecimal)val;
-                     newBid.setAttribute("Bidderid",id);
-                    newBid.setAttribute("Amount",amou);
-                    newBid.setAttribute("Auctionid",aid);
-    newBid.setAttribute("Bidtime",time);
+
+                    
+                    newBid.setAttribute("Bidderid",id);
+                    newBid.setAttribute("Amount",amount);
+                    newBid.setAttribute("Auctionid",auctionid);
+                    newBid.setAttribute("Bidtime",time);
+                    auction.setAttribute("Currentprice",amount);
+                    
                             try {
+                                    AuctionVo.setCurrentRow(auction);
                                     BidVO.insertRow(newBid);
                                     am.getTransaction().commit();
                                     
@@ -95,58 +102,43 @@ public class newbidentry extends LoggedUserHelper{
                                     er.printStackTrace();
                                     FacesContext.getCurrentInstance().addMessage(null,
                                             new FacesMessage(FacesMessage.SEVERITY_INFO, "Creds not added!", null));
-    }
-    return null;
-}
-
-    /*public void OKentrybid(AttributeChangeEvent attributeChangeEvent) {
-        // Add event code here...
-        BindingContext bctx = BindingContext.getCurrent();
-                    DCBindingContainer bindings = (DCBindingContainer) bctx.getCurrentBindingsEntry();
-                    DCDataControl dataControl = bindings.getDataControl();
-                    ApplicationModule am = (ApplicationModule)dataControl.getDataProvider();
-                    ViewObject BidVO = am.findViewObject("G4BidVO2");
-                    Row newBid = BidVO.createRow();
-
-                    bidderid = getLoggedInUserId();
-                    time= Timestamp.valueOf(LocalDateTime.now());
-                    bidderId1 = BigDecimal.valueOf(bidderid);
-                     newBid.setAttribute("Bidderid",bidderId1);
-                    newBid.setAttribute("Amount",amount);
-                    newBid.setAttribute("Auctionid",auctionId);
-        newBid.setAttribute("Bidtime",time);
-                            try {
-                                    BidVO.insertRow(newBid);
-                                    am.getTransaction().commit();
-
-                                } catch (Exception er) {
-                                    er.printStackTrace();
-                                    FacesContext.getCurrentInstance().addMessage(null,
-                                            new FacesMessage(FacesMessage.SEVERITY_INFO, "Creds not added!", null));
         }
-
-    }*/
-    public void setBidderId(RichInputText bidderId) {
-        this.bidderId = bidderId;
+        
+        RichPopup popup  = getMyPopup();
+        popup.hide();
+        return null;
     }
 
-    public RichInputText getBidderId() {
-        return bidderId;
+    public void setMyPopup(RichPopup myPopup) {
+        this.myPopup = myPopup;
     }
 
-    public void setAmount(RichInputText amount) {
+    public RichPopup getMyPopup() {
+        return myPopup;
+    }
+
+    public void setAmount(int amount) {
         this.amount = amount;
     }
 
-    public RichInputText getAmount() {
+    public int getAmount() {
         return amount;
     }
 
-    public void setAuctionid(RichInputText auctionid) {
-        this.auctionid = auctionid;
+    public void setTime(Timestamp time) {
+        this.time = time;
     }
 
-    public RichInputText getAuctionid() {
-        return auctionid;
+    public Timestamp getTime() {
+        return time;
     }
+
+    public void setBidderid(Integer bidderid) {
+        this.bidderid = bidderid;
+    }
+
+    public Integer getBidderid() {
+        return bidderid;
+    }
+
 }
